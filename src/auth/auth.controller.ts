8027fb94801fe496791af4692b84f250d52fefa1
Jereme <jeremejazz15@gmail.com>
2025-04-11 23:21:08 +0800

    chore: change code order

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,17 +10,17 @@ import { Public } from './decorators/public.decorator';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('signin')
-  @Public()
-  async signIn(@Body() signInDTO: SignInDTO) {
-    return await this.authService.signIn(signInDTO);
-  }
-
   @Post('signup')
   @Public()
   async signUp(@Body() signupDto: CreateUserDto): Promise<User | null> {
     const user = await this.authService.signUp(signupDto);
 
     return user;
+  }
+
+  @Post('signin')
+  @Public()
+  async signIn(@Body() signInDTO: SignInDTO) {
+    return await this.authService.signIn(signInDTO);
   }
 }
